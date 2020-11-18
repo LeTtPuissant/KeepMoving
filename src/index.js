2a47255162c4c2dfdd20f94264e2d1e35bd58041
Jourdain KIMB'S <jourdainkimbembe530@yahoo.fr>
2020-11-18 09:44:37 +0100

    front

---
+++ src/index.js
@@ -0,0 +1,9 @@
+if("serviceWorker" in navigator){
+    navigator.serviceWorker.register("sw.js").then(registration =>{
+        console.log("Service Worker Enregistré");
+        console.log(registration);
+    }).catch(error => {
+        console.log("Service Worker Enregistrement échoué");
+        console.log(error);
+    });
+}
